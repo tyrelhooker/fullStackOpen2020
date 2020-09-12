cc30d981b6297f4e42ede76258c7c34f1f73a4e1
TJ Hooker <tyrelhooker@gmail.com>
2020-09-11 22:54:41 -0400

    Refactor code to implement multiple components and destructuring props

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const Header = ({courseName}) => <h1>{courseName}</h1>;
+// TODO: const Content
+const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => ( 
+  <>
+    <p>
+      {part1} {exercises1}
+    </p>
+    <p>
+      {part2} {exercises2}
+    </p>
+    <p>
+      {part3} {exercises3}
+    </p>
+  </>  
+)
+
+// TODO: const Total
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header courseName={course} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
-  );
+  )
 }
 
 export default App;
