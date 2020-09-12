10d8cd10287e3e7d59b5751b5e1c5b30a0d0b756
TJ Hooker <tyrelhooker@gmail.com>
2020-09-12 15:48:59 -0400

    Finish step 5 by combining data into a single object and refactoring app

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,41 +1,68 @@
 import React from 'react';
 
-const Header = ({ courseName }) => <h1>{courseName}</h1>;
+const Header = ({course}) => {
+  console.log(course);
 
-const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => ( 
-  <>
-    <Part partNum={part1} exerciseNum={exercises1} />
-    <Part partNum={part2} exerciseNum={exercises2} />
-    <Part partNum={part3} exerciseNum={exercises3} />
-  </>  
-)
+  return (
+    <h1>{course.name}</h1>
+  )
+}
 
-const Part = ({ partNum, exerciseNum }) => (
+
+
+const Content = ({ course }) => {
+  console.log(course);
+  return ( 
+    <>
+      <Part parts={course.parts} />
+    </>  
+  )
+}
+
+const Part = ({parts}) => (
   <>
-    <p>
-      {partNum}: {exerciseNum}
-    </p>
+    {parts.map(part => <p key={part.name}>{part.name}: {part.exercises}</p>)}
   </>
 )
 
 
-const Total = ({ exercises1, exercises2, exercises3 }) => <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+const Total = ({ exercises }) => {
+  console.log(exercises);
 
-const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+  let totalExercises = 0;
+  exercises.forEach(exercise => totalExercises += exercise)
 
   return (
-    <div>
-      <Header courseName={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-    </div>
+    <p>Number of exercises: {totalExercises} </p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name:'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name:'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      },
+    ]
+  }
+
+
+  return (
+    <>
+      <Header course={course} />
+      <Content course={course} />
+      <Total exercises={course.parts.map(part => part.exercises)} />
+    </>
   )
 }
 
