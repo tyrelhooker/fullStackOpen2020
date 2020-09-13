17beb35a25c77d1541cfc8e3e1865fe2cc6d55b8
TJ Hooker <tyrelhooker@gmail.com>
2020-09-13 13:43:39 -0400

    Add counter component and logic to increment counter

--- part1/part1_app/src/App.js
+++ part1/part1_app/src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import Hello from './components/Hello.js'
+import React, { useState } from 'react';
+import Hello from './components/Hello.js';
+import Counter from './components/Counter.js';
 
 const App = () => {
+
+  const [counter, setCounter] = useState(0);
 
   const personInfo = {
     name: 'Peter', 
     age: '10'
   }
 
+  if (counter < 5) {
+    setTimeout( 
+      () => setCounter(counter + 1),
+      1000
+    )  
+  }
+ 
+
   return (
     <>
       <p>Hello World</p>
       <Hello personInfo={personInfo} />
+      <Counter counter={counter} />
     </>
   )
 }
---
+++ part1/part1_app/src/components/Counter.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Counter = ({ counter }) => <><p>{counter}</p></>;
+
+export default Counter;
