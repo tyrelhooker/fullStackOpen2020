2e9c92fd98399e55df8a7af4f5376ffc79802a2c
TJ Hooker <tyrelhooker@gmail.com>
2020-09-15 23:21:45 -0400

    Clean up code. Make random function in App.js more DRY

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -7,8 +7,8 @@ import BestAnecdote from './components/BestAnecdote';
 const anecdotes = ["If it hurts, do it more often",'Adding manpower to a late software project makes it later!','The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.','Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', 'Premature optimization is the root of all evil.', 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ];
 
-const random = () => {
-  let maxNumber = anecdotes.length;
+const random = (array) => {
+  let maxNumber = array.length;
   let randomNumber = Math.floor((Math.random() * maxNumber));
 
   return (
@@ -27,7 +27,7 @@ const App = () => {
     5: 0 
   });
   
-  const handleRandom = () => setSelected(random);  
+  const handleRandom = () => setSelected(random(anecdotes));  
 
   const handlePoints = () => {
     const copy = { ...points };
@@ -44,7 +44,7 @@ const App = () => {
       <VoteButton handlePoints={handlePoints}/>
       <br />
 
-      <h2>Anecdote with Most Votes</h2>
+      <h2>Most popular Anecdote</h2>
       <BestAnecdote points={points} anecdotes={anecdotes} />
     </div>
   )
--- part1/anecdotes/src/components/BestAnecdote.js
+++ part1/anecdotes/src/components/BestAnecdote.js
@@ -4,47 +4,20 @@ const BestAnecdote = ({ points, anecdotes}) => {
   console.log(points);
   console.log(anecdotes);
 
-  const entries = Object.entries(points);
-  console.log(entries);
-
   let theHighest = [0, 0];
-  const highest = entries.forEach(([key, value]) => {
-    // console.log(key, value);
+  
+  Object.entries(points).forEach(([key, value]) => {
     if (value > theHighest[1]) {
       theHighest = [key, value];
-      
     }
-    console.log('theHighest', theHighest)
   })
-
-  // console.log('highest', highest);
 
   return (
     <>
-      {anecdotes[theHighest[0]]}
+      <p>{anecdotes[theHighest[0]]}</p>
       <p>Votes: {theHighest[1]}</p>
     </>
   )
-
-  // let pointsArr = Object.values(points);
-  // console.log(pointsArr);
-
-  // let mapPointsArr = pointsArr.reduce((prev, curr) => {
-  //   return Math.max(prev, curr);
-  // })
-
-  // let highestAnecdote = Object.keys(points).reduce((a, b) => {
-  //   points[a] > points[b] ? anecdotes[a] : anecdotes[b])
-
-
-  // return (
-  //   <>
-  //   <p>Anecdote here</p>
-  //   <p>{mapPointsArr}</p>
-
-  //   <p>{highestAnecdote}</p>
-  //   </>
-  // )
 }
 
 
